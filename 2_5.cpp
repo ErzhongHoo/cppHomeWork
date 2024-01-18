dee8516211e063d443602263acb150c3b5e96cbe
ezhonghu <1942218829@qq.com>
2024-01-18 21:35:22 +0800

    add 2_5

---
+++ 2_5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+char matrix[80][80];
+
+// 遍历matrix矩阵中的元素
+
+
+int main(){
+    int n;
+    char A, B;
+    cin >> n >> A >> B;
+
+
+}
